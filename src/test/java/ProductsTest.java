ad0f39eb8cdd98b57c442af0d88eb703c0afd4bf
Jed Duffey <jedduffey@gmail.com>
2018-08-03 08:01:39 -0400

    Clean up old/unused tests

--- src/test/java/ProductsTest.java
+++ src/test/java/ProductsTest.java
@@ -19,30 +19,4 @@ public class ProductsTest {
         Assert.assertEquals(738, session.getPreTaxTotal());
     }
 
-    //    @Test
-//    public void perUnitItemsShouldHaveGettersForPricePerUnitAndProductType() {
-//        Assert.assertEquals(189, Products.getPrice("soup"));
-//        Assert.assertEquals(79, Products.getPrice("snickers"));
-//        Assert.assertEquals(599, Products.getPrice("six_pack_beer"));
-//    }
-//
-//    @Test
-//    public void scanningAnItemShouldAddThatItemsPriceToCheckoutTotal() {
-//        CheckoutSession session = new CheckoutSession();
-//        Assert.assertEquals(0, session.getPreTaxTotal());
-//        session.scanItem("soup");
-//        Assert.assertEquals(189, session.getPreTaxTotal());
-//        session.scanItem("snickers");
-//        Assert.assertEquals(189 + 79, session.getPreTaxTotal());
-//        session.scanItem("six_pack_beer");
-//        Assert.assertEquals(189 + 79 + 599, session.getPreTaxTotal());
-//    }
-//
-//    @Test
-//    public void perWeightItemsShouldHaveGettersForTheirPerPerPoundPrice() {
-//        Assert.assertEquals(399, Products.getPrice("beef"));
-//        Assert.assertEquals(159, Products.getPrice("bananas"));
-//        Assert.assertEquals(799, Products.getPrice("ground_coffee"));
-//    }
-
 }
